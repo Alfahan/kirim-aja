edac7445c5c7a134483146396da07dd29b459196
Alfahan <ali.farhan160@gmail.com>
2025-10-16 13:47:11 +0700

    feat: api-register

--- kirimaja-be/src/modules/auth/auth.module.ts
+++ kirimaja-be/src/modules/auth/auth.module.ts
@@ -1,9 +1,8 @@
-import { Module } from "@nestjs/common";
-import { AuthController } from "./auth.controller";
-import { AuthService } from "./auth.service";
-import { PrismaService } from "src/common/prisma/prisma.service";
-import { JwtService } from "@nestjs/jwt";
-
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { PrismaService } from 'src/common/prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
     imports: [],
