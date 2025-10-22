1dbeb6d82de8b289a4d5a0997ef09f69caac3d0b
Alfahan <ali.farhan160@gmail.com>
2025-10-22 08:24:08 +0700

    feat: get-update-profile

--- kirimaja-be/src/app.module.ts
+++ kirimaja-be/src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthModule } from './modules/auth/auth.module';
 import { RolesModule } from './modules/roles/roles.module';
 import { PermissionsModule } from './modules/permissions/permissions.module';
 import { JwtAuthGuard } from './modules/auth/guard/logged-in-guard';
+import { ProfileModule } from './modules/profile/profile.module';
 
 @Module({
-    imports: [AuthModule, RolesModule, PermissionsModule],
+    imports: [AuthModule, RolesModule, PermissionsModule, ProfileModule],
     controllers: [AppController],
     providers: [AppService, JwtAuthGuard],
 })
